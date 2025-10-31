a9010e310730e6dfa5a5c8b20b4fa222de1e1213
Jesaiah Prayor <jesaiahprayor@jesaiahs-mbp.mynetworksettings.com>
2025-10-31 14:34:56 -0400

    Added hero component to render video

---
+++ app/components/Hero.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+const Hero = () => {
+    return (
+        <div className="App">
+        <video width="750" height="500" controls >
+        <source src="/Comecial.mp4" type="video/mp4"/>
+        </video>
+        </div>
+        );
+}
+
+export default Hero
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Hero from "./components/Hero"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div>Home page</div>
-    </div>
+    <>
+      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <Hero/>
+        <div>Home page</div>
+      </div>
+    </>
+
   );
 }
