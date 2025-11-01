b4ddfc25f7c8ea707de4e2d6b4719f009b7bbaf9
Jesaiah Prayor <jesaiahprayor@jesaiahs-mbp.mynetworksettings.com>
2025-11-01 16:20:55 -0400

    start video at a certain time, include autoplay

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,9 +1,33 @@
 'use client';
+import {useRef, useEffect } from "react";
 
 const Hero = () => {
+    const videoRef = useRef(null)
+    const startTime = 7; // video start time
+    useEffect(()=>{
+        const video = videoRef.current
+
+        if (!video){
+            return
+        }
+        const handleVideo = () => {
+            video.currentTime = startTime
+        }
+
+        video.addEventListener("loadedmetadata", handleVideo)
+        return () => {
+            video.removeEventListener('loadedmetadata', handleVideo)
+        }
+    },[])
     return (
         <div className="App">
-        <video width="750" height="500" controls >
+        <video 
+            ref={videoRef}
+            width="750" 
+            height="500"
+            controls
+            muted
+        >
         <source src="/Comecial.mp4" type="video/mp4"/>
         </video>
         </div>
